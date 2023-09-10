0030d92f6a6c71c29f7997332fb3d3ce8631f39a
Quillinan <milton.quillinan1798@gmail.com>
2023-09-09 22:08:13 -0300

    fix create flight

--- src/controllers/flights.controller.js
+++ src/controllers/flights.controller.js
@@ -6,13 +6,19 @@ export const flightController = {
       const { origin, destination, date } = req.body;
 
       const flightService = new FlightService();
-      const newFlight = await flightService.createFlight(
+      const result = await flightService.createFlight(
         origin,
         destination,
         date
       );
 
-      res.status(201).json(newFlight);
+      if (result.status === 409) {
+        res.status(409).json({ error: result.message });
+      } else if (result.status === 404) {
+        res.status(404).json({ error: result.message });
+      } else {
+        res.status(201).json(result);
+      }
     } catch (error) {
       console.error("Erro ao criar voo:", error);
       res.status(500).json({ error: "Erro interno do servidor" });
--- src/repositories/flights.repositories.js
+++ src/repositories/flights.repositories.js
@@ -3,6 +3,29 @@ import connection from "../database/database.connection.js";
 export const FlightRepository = {
   createFlight: async (origin, destination, date) => {
     try {
+      const origemValida = await verificarExistenciaCidade(origin);
+      const destinoValido = await verificarExistenciaCidade(destination);
+
+      if (origin === destination) {
+        return {
+          status: 409,
+          message: "Origem e destino devem ser diferentes",
+        };
+      }
+
+      if (!origemValida) {
+        return {
+          status: 404,
+          message: "Origem inválida",
+        };
+      }
+      if (!destinoValido) {
+        return {
+          status: 404,
+          message: "Destino inválida",
+        };
+      }
+
       const query =
         "INSERT INTO flights (origin, destination, date) VALUES ($1, $2, $3) RETURNING *";
       const values = [origin, destination, date];
@@ -13,3 +36,15 @@ export const FlightRepository = {
     }
   },
 };
+
+async function verificarExistenciaCidade(cityId) {
+  try {
+    const query = "SELECT COUNT(*) FROM cities WHERE id = $1";
+    const values = [cityId];
+    const result = await connection.query(query, values);
+
+    return result.rows[0].count > 0;
+  } catch (error) {
+    throw error;
+  }
+}
